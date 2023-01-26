ecbdbad4e2819740e9bb336517682be1bcbcb4fe
A.S.Rawat <adityasrwt5@gmail.com>
2023-01-26 14:31:33 +0530

    Addition of enemy

--- Bubble Struggle/Assets/Script/Enemy.cs
+++ Bubble Struggle/Assets/Script/Enemy.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject preFab1;
+    //[SerializeField] private int speed = 7;
     //[SerializeField] private GameObject preFab2;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Enemy")
+        if (other.gameObject.tag == "bullet")
         {
             splitBubble();
         }
@@ -18,8 +19,8 @@ public class Enemy : MonoBehaviour
 
     private void splitBubble()
     {
-        Instantiate(preFab1, transform.position, Quaternion.identity);
+        GameObject bubblee = Instantiate(preFab1, transform.position, Quaternion.identity);
+        //bubblee.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0), ForceMode2D.Impulse);
         Destroy(gameObject);
-        //Instantiate(preFab2, transform.position, Quaternion.identity);
     }
 }
